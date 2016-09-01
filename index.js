4f6a9b9d8143b9849cbb76a34debd6b1dd0b082b
Sol Irvine <sol@turnstilelive.com>
2016-09-01 13:05:13 +0900

    downgrade node version

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+/* jshint node: true */
+'use strict';
+
 module.exports = {
   name: 'ember-ahoy',
+  isDevelopingAddon: function() {
+    return process.env.ADDON_DEVELOPMENT === 'true';
+  },
   included: function(app){
     this._super.included(app);
-
     app.import(app.bowerDirectory + '/ahoy/ahoy.js');
   },
   contentFor: function(target, config) {
     if (target === 'head') {
-      return `<script>
-                window.ahoy = {};
-                window.ahoy.visitsUrl = '${config.ahoy.visitsUrl}';
-                window.ahoy.eventsUrl = '${config.ahoy.eventsUrl}';
-              </script>`;
+      return `<script>window.ahoy = {};\nwindow.ahoy.visitsUrl = '${config.ahoy.visitsUrl}';\nwindow.ahoy.eventsUrl = '${config.ahoy.eventsUrl}';</script>`;
     }
   }
 };
